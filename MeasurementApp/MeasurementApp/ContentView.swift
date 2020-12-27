b87b20b7bed48e04002386eb163ed575c04370b8
Felipe Marino <felipemarino91@gmail.com>
2020-12-28 00:35:27 +0100

    Add empty measurement app

---
+++ MeasurementApp/MeasurementApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MeasurementApp
+//
+//  Created by Marino Felipe on 27.12.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MeasurementApp/MeasurementApp/MeasurementApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeasurementApp.swift
+//  MeasurementApp
+//
+//  Created by Marino Felipe on 27.12.20.
+//
+
+import SwiftUI
+
+@main
+struct MeasurementAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
