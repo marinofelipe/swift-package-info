1e42355e512add909a4ee236f29751dbb2000403
Felipe Marino <felipemarino91@gmail.com>
2021-01-19 00:58:03 +0100

    Add some other functional test cases

--- Tests/RunTests/RunTests.swift
+++ Tests/RunTests/RunTests.swift
@@ -6,52 +6,122 @@
 //
 
 import XCTest
+import Foundation
 
+@available(macOS 10.13, *)
 final class RunTests: XCTestCase {
-    func testHelp() throws {
-        guard #available(macOS 10.13, *) else { return }
+    func testWithInvalidRepositoryURL() throws {
+        try runToolProcessAndAssert(
+            command: "--repository-url somethingElse --package-version 6.0.0 --product RxSwift",
+            expectedOutput: """
+            Error: Invalid argument \'--repository-url <repository-url>\'\nUsage: The URL must be a valid git repository URL that contains a `Package.swift`, e.g. `https://github.com/Alamofire/Alamofire`.
 
-        let executableURL = productsDirectory.appendingPathComponent("swift-package-info")
+            """,
+            expectedError: ""
+        )
+    }
 
-        let process = Process()
-        process.executableURL = executableURL
-        process.arguments = ["--help"]
+    func testWithInexistentTag() throws {
+        try runToolProcessAndAssert(
+            command: "--repository-url https://github.com/ReactiveX/RxSwift --package-version 8.0.0 --product RxSwift",
+            expectedOutput: """
+            [1/6] Generating archive for empty app...
+            [2/6] Calculating binary size...
+            [3/6] Adding RxSwift as dependency...
+            [4/6] Generating archive for updated app...
+            [5/6] Calculating updated binary size...
+            [6/6] Reseting app...
 
-        let outputPipe = Pipe()
-        process.standardOutput = outputPipe
+            """,
+            expectedError: ""
+        )
+    }
 
-        let errorPipe = Pipe()
-        process.standardError = errorPipe
+    func testWithInvalidProduct() throws {
+        try runToolProcessAndAssert(
+            command: "--repository-url https://github.com/ReactiveX/RxSwift --package-version 8.0.0 --product some",
+            expectedOutput: """
+            Error: Invalid argument \'--product <product>\'
+            Usage: The product should match one of the declared products on https://github.com/ReactiveX/RxSwift.
+            Found available products: [\"RxSwift\", \"RxCocoa\", \"RxRelay\", \"RxBlocking\", \"RxTest\"].
 
-        try process.run()
-        process.waitUntilExit()
+            """,
+            expectedError: ""
+        )
+    }
 
-        let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
-        let outputContent = String(data: outputData, encoding: .utf8)
+    func testWhenAllParametersAreValid() throws {
+        try runToolProcessAndAssert(
+            command: "--repository-url https://github.com/ReactiveX/RxSwift --package-version 6.0.0 --product RxSwift",
+            expectedOutput: """
+            [1/6] Generating archive for empty app...
+            [2/6] Calculating binary size...
+            [3/6] Adding RxSwift as dependency...
+            [4/6] Generating archive for updated app...
+            [5/6] Calculating updated binary size...
+            [6/6] Reseting app...
 
-        let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
-        let errorContent = String(data: errorData, encoding: .utf8)
+            """,
+            expectedError: ""
+        )
+    }
 
-        XCTAssertEqual(
-            outputContent,
-            """
-            OVERVIEW: A tool for analyzing Swift Packages
+    func testWhenVerboseFlagIsSet() throws {
+        // All values are expected to be logged via Console
+        try runToolProcessAndAssert(
+            command: "--repository-url https://github.com/ReactiveX/RxSwift --package-version 6.0.0 --product RxSwift --verbose",
+            expectedOutput: "",
+            expectedError: ""
+        )
+    }
 
-            Provides valuable information about a given Swift Package,\nthat can be used in your favor when deciding whether to\nadopt or not a Swift Package as a dependency on your app.
-
-            USAGE: swift-package-info <subcommand>
-
-            OPTIONS:
-              --version               Show the version.
-              -h, --help              Show help information.\n\nSUBCOMMANDS:
-              binary-size             Check the estimated size of a Swift Package.
-              full-analyzes (default) Get all available information about a Swift Package
-
-              See \'swift-package-info help <subcommand>\' for detailed help.
+    func testWithMissingParameters() throws {
+        // assert when only --repository-url is passed in
+        try runToolProcessAndAssert(
+            command: "--repository-url https://github.com/ReactiveX/RxSwift",
+            expectedOutput: "",
+            expectedError: """
+            Error: Missing expected argument \'--package-version <package-version>\'\nUsage: swift-package-info full-analyzes --repository-url <repository-url> --package-version <package-version> --product <product> [--verbose]
+              See \'swift-package-info full-analyzes --help\' for more information.
 
             """
         )
-        XCTAssertEqual(errorContent, "")
+
+        // assert when --product is missing
+        try runToolProcessAndAssert(
+            command: "--repository-url https://github.com/ReactiveX/RxSwift --package-version 6.0.0",
+            expectedOutput: "",
+            expectedError: """
+            Error: Missing expected argument \'--product <product>\'\nUsage: swift-package-info full-analyzes --repository-url <repository-url> --package-version <package-version> --product <product> [--verbose]
+              See \'swift-package-info full-analyzes --help\' for more information.
+
+            """
+        )
+    }
+
+    func testHelp() throws {
+        let expectedOutput = """
+        OVERVIEW: A tool for analyzing Swift Packages
+
+        Provides valuable information about a given Swift Package,\nthat can be used in your favor when deciding whether to\nadopt or not a Swift Package as a dependency on your app.
+
+        USAGE: swift-package-info <subcommand>
+
+        OPTIONS:
+          --version               Show the version.
+          -h, --help              Show help information.\n\nSUBCOMMANDS:
+          binary-size             Check the estimated size of a Swift Package.
+          full-analyzes (default) Get all available information about a Swift Package
+
+          See \'swift-package-info help <subcommand>\' for detailed help.
+
+        """
+
+        try runToolProcessAndAssert(
+            command: "--help",
+            expectedOutput: expectedOutput,
+            expectedError: ""
+        )
     }
 
     /// Returns path to the built products directory.
@@ -69,4 +139,49 @@ final class RunTests: XCTestCase {
     static var allTests = [
         ("testHelp", testHelp),
     ]
+
+    // MARK: - Helpers
+
+    private func runToolProcessAndAssert(
+        _ file: StaticString = #file,
+        _ function: StaticString = #function,
+        _ line: UInt = #line,
+        command: String,
+        expectedOutput: String,
+        expectedError: String
+    ) throws {
+        let commands = command.split(whereSeparator: \.isWhitespace)
+
+        let arguments: [String]
+        if commands.count > 1 {
+            arguments = commands.map { String($0) }
+        } else {
+            arguments = command
+                .split { [" -", " --"].contains(String($0)) }
+                .map { String($0) }
+        }
+
+        let executableURL = productsDirectory.appendingPathComponent("swift-package-info")
+
+        let process = Process()
+        process.executableURL = executableURL
+        process.arguments = arguments
+
+        let outputPipe = Pipe()
+        process.standardOutput = outputPipe
+
+        let errorPipe = Pipe()
+        process.standardError = errorPipe
+
+        try process.run()
+        process.waitUntilExit()
+
+        let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+        let outputContent = String(data: outputData, encoding: .utf8)
+        let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
+        let errorContent = String(data: errorData, encoding: .utf8)
+
+        XCTAssertEqual(outputContent, expectedOutput)
+        XCTAssertEqual(errorContent, expectedError)
+    }
 }
