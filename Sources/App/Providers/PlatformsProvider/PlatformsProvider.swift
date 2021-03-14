94dcd96bb57293d631548efab4101cbe3b1d5df5
Felipe Marino <felipemarino91@gmail.com>
2021-03-14 19:28:58 +0100

    Update PlatformsProvider and cover it with base test

--- Sources/App/Providers/PlatformsProvider/PlatformsProvider.swift
+++ Sources/App/Providers/PlatformsProvider/PlatformsProvider.swift
@@ -13,19 +13,49 @@ public struct PlatformsProvider {
         packageContent: PackageContent,
         verbose: Bool
     ) -> Result<ProvidedInfo, InfoProviderError> {
-        let messages: [ConsoleMessage]
-        if packageContent.platforms.isEmpty {
-            messages = [
+        .success(
+            .init(
+                providerName: "Platforms",
+                information: PlatformsInformation(
+                    platforms: packageContent.platforms
+                )
+            )
+        )
+    }
+}
+
+struct PlatformsInformation: Equatable, Encodable, CustomConsoleMessagesConvertible {
+    let platforms: [PackageContent.Platform]
+    let iOS: String?
+    let macOS: String?
+    let tvOS: String?
+    let watchOS: String?
+
+    var messages: [ConsoleMessage] { buildConsoleMessages() }
+
+    init(platforms: [PackageContent.Platform]) {
+        self.platforms = platforms
+        self.iOS = platforms.iOSVersion
+        self.macOS = platforms.macOSVersion
+        self.tvOS = platforms.tvOSVersion
+        self.watchOS = platforms.watchOSVersion
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case iOS, macOS, tvOS, watchOS
+    }
+
+    private func buildConsoleMessages() -> [ConsoleMessage] {
+        if platforms.isEmpty {
+            return [
                 .init(
                     text: "System default",
                     hasLineBreakAfter: false
                 )
             ]
         } else {
-            messages = packageContent.platforms
+            return platforms
                 .map { platform -> [ConsoleMessage] in
-                    let isLastPlatforms = platform == packageContent.platforms.last
-
                     var messages = [
                         ConsoleMessage(
                             text: "\(platform.platformName) from v. \(platform.version)",
@@ -34,7 +64,8 @@ public struct PlatformsProvider {
                         )
                     ]
 
-                    if isLastPlatforms == false {
+                    let isLastPlatform = platform == platforms.last
+                    if isLastPlatform == false {
                         messages.append(
                             .init(
                                 text: " | ",
@@ -50,12 +81,23 @@ public struct PlatformsProvider {
                     +
                 )
         }
+    }
+}
 
-        return .success(
-            .init(
-                providerName: "Platforms",
-                messages: messages
-            )
-        )
+extension Array where Element == PackageContent.Platform {
+    var iOSVersion: String? {
+        first(where: \.platformName == "ios")?.version
+    }
+
+    var macOSVersion: String? {
+        first(where: \.platformName == "macos")?.version
+    }
+
+    var tvOSVersion: String? {
+        first(where: \.platformName == "tvos")?.version
+    }
+
+    var watchOSVersion: String? {
+        first(where: \.platformName == "watchos")?.version
     }
 }
