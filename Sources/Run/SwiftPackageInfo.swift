e17da3883cfeaf07f17c4694f357472ca1606f76
Felipe Marino <felipemarino91@gmail.com>
2025-10-09 22:09:45 +0200

    Reuse version definition

--- Sources/Run/SwiftPackageInfo.swift
+++ Sources/Run/SwiftPackageInfo.swift
@@ -37,7 +37,7 @@ public struct SwiftPackageInfo: AsyncParsableCommand {
     that can be used in your favor when deciding whether to
     adopt or not a Swift Package as a dependency on your app.
     """,
-    version: "1.6.0",
+    version: .defaultVersion,
     subcommands: [
       BinarySize.self,
       Platforms.self,
